b357692bb214c6278e560689942669a20de89f21
Fabian Fröschl <fabianf96@googlemail.com>
2022-02-25 11:58:22 +0100

    Changes in test_mainMenu.py

--- src/game/test_mainMenu.py
+++ src/game/test_mainMenu.py
@@ -3,6 +3,7 @@ import io
 import sys
 import mainMenu
 
+
 class testMainMenu(unittest.TestCase):
     def test_preloop(self):
         captured_output = io.StringIO()
@@ -12,4 +13,15 @@ class testMainMenu(unittest.TestCase):
         res = captured_output.getvalue()
         exp = "\n"
         self.assertEqual(exp, res)
-        
+
+    def test_do_exit(self):
+        res = mainMenu.MainMenu().do_exit(line="exit")
+        self.assertTrue(res)
+
+    def test_do_exit_terminates(self):
+        mainMenu.MainMenu().cmdloop()
+        mainMenu.MainMenu().do_exit(line="exit")
+        self.assertIsNone(None)
+
+if __name__ == "__main__":
+    unittest.main()
