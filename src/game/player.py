3ef63c53fc548ba314a7df5f8401b3b88aec8827
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-02-27 13:52:11 +0100

    small update #2

---
+++ src/game/player.py
@@ -0,0 +1,7 @@
+class player: 
+    def __init__(self, playerId, numOfRounds, totalPlays, totalWins):
+        self.playerId = playerId
+        self.numOfRounds = numOfRounds
+        self.totalPlays = totalPlays
+        self.totalWins = totalWins
+        
