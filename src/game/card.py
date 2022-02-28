2dbbe75b5cb3a5476496386dcecd37fd0eda6a10
enzotiberghien <enzotiberghien28@gmail.com>
2022-02-28 09:11:57 +0100

    commit deck

--- src/game/card.py
+++ src/game/card.py
@@ -3,3 +3,6 @@ class Card:
         self.number = number
         self.suit = suit
         self.value = value
+
+    def __str__(self):
+        return f"{self.suit} {self.number}"
--- src/game/cardHand.py
+++ src/game/cardHand.py
@@ -2,4 +2,4 @@ import deck
 
 class CardHand: 
     def __init__(self, deck, i): 
-        self.cardHand = [x for x in deck if list(deck).index(x) % 2 == i]
+        self.cardHand = [x for x in list(deck) if list(deck).index(x) % 2 == i]
