bc7d4522ae242279e29e7ee7747ab1f2db7e8363
Froschi1860 <fabianf96@googlemail.com>
2022-03-02 13:19:44 +0100

    Finalized player menu + test suite, worked on main menu

--- src/game/playerMenu.py
+++ src/game/playerMenu.py
@@ -1,12 +1,9 @@
 import cmd, player
-from encodings import normalize_encoding
 
 
 class PlayerMenu(cmd.Cmd):
     
     prompt = "\n(Player menu) "
-
-    player = None
 
     menu_message = """
 Player menu
@@ -14,22 +11,28 @@ Player menu
 
 Please choose one of the folowing options:
 
-choose - Open an existing player profile
-create - Create a new player profile
-id - Change id of current player
+choose <player_id> - Open an existing player profile
+create <player_id> - Create a new player profile
+id <player_id> - Change id of current player
 current - Display current Player
 menu - Display menu
 exit - Return to main menu"""
 
-    def __init__(self, completekey='tab', stdin: str=None, stdout: str=None, player: player=None):
-        '''Initialise a PlayerMenu object and sets self.player is a player is already chosen'''
+    def __init__(self, completekey='tab', stdin=None, stdout=None, player: player=None, test_mode=False, test_cmd=""):
+        '''Initialise a PlayerMenu object and enable test mode for cmdloop()'''
         super().__init__(completekey, stdin, stdout)
         self.player = player
+        self.test_mode = test_mode
+        self.test_cmd = test_cmd
 
     def cmdloop(self):
-        '''Run menu and return current player'''
-        super().cmdloop()
-        return self.player
+        '''Run menu and return current player in game mode'''
+        if self.test_mode:
+            self.onecmd(self.test_cmd)
+            self.onecmd("exit")
+        else:
+            super().cmdloop()
+            return self.player
 
     def precmd(self, line: str):
         '''Assure case-insensitivity'''
@@ -41,30 +44,35 @@ exit - Return to main menu"""
 
     def do_choose(self, line):
         '''Open an existing player profile and display chosen player'''
-        player_id = input("Please enter the player id: ")
-        if not player.check_player_id(player_id):
-            print("The chosen player does not exist")
+        player_id = line
+        if player_id == "":
+            print("Enter a player id to choose a player: choose <player_id>")
+        elif not player.check_player_id(player_id):
+            print(f"The player with the id {player_id} does not exist")
         else:
-            self.player = player.choose_player()
-        self.onecmd(line="current")
+            self.player = player.choose_player(player_id)
 
     def do_create(self, line):
         '''Create a new player profile'''
-        new_player_id = input("Please enter the id of the new player: ")
-        if player.check_player_id(new_player_id):
-            print("A player with this id already exists.")
+        new_player_id = line
+        if new_player_id == "":
+            print("Enter a player id to create a player: create <player_id>")
+        elif player.check_player_id(new_player_id):
+            print(f"A player with the id {new_player_id} already exists.")
         else:
             self.player = player.Player(new_player_id)
-        self.onecmd(line="current")
 
     def do_id(self, line):
         '''Change id of current player'''
-        new_player_id = input("Please enter the new id: ")
-        if player.check_player_id(new_player_id):
-            print("A player with the chosen id already exists")
+        new_player_id = line
+        if new_player_id == "":
+            print("Enter a player id to change the id of the current player: id <player_id>")
+        elif self.player == None:
+            print("Choose or create a player before changing the id")
+        elif player.check_player_id(new_player_id):
+            print(f"A player with the id {new_player_id} already exists")
         else:
             self.player = player.change_player_id(new_player_id)
-        self.onecmd(line="current")
 
     def do_current(self, line):
         '''Display current player'''
@@ -83,3 +91,6 @@ exit - Return to main menu"""
             print("\nChoose or create a player to continue")
         else:
             return True
+
+    def do_test(self, line):
+        print(line)
--- src/game/test_mainMenu.py
+++ src/game/test_mainMenu.py
@@ -2,19 +2,12 @@ import unittest
 import io
 import sys
 import mainMenu
+import player
 
 
 class testMainMenu(unittest.TestCase):
+    pass
 
-    def test_do_end(self):
-        res = mainMenu.MainMenu().do_end(line="exit")
-        self.assertTrue(res)
-
-    def test_do_exit_terminates_menu(self):
-        menu = mainMenu.MainMenu()
-        menu.cmdloop()
-        res = menu.onecmd("end")
-        self.assertTrue(res)
 
 if __name__ == "__main__":
     unittest.main()
