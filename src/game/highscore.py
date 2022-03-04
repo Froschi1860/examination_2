b35ac8855ca2d959e3ad75d859c766aa9424a4d6
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2022-03-04 08:25:34 +0100

    update--

--- src/game/highscore.py
+++ src/game/highscore.py
@@ -1,14 +1,29 @@
+
 import Player
 
 class Highscore:
     
-    format = '''HIGHSCORE RESULTS:\n
-    ------------------------'''
     
     def __init__(self, player_list):
         self.player_list = player_list
     
     def sort_score_results(self):
-        return sorted(self.player_list, key=lambda x: x['Total Games Won'])
-  
-    
+        scoreboard = []
+        ranked_list_of_dicts = sorted(self.player_list, key=lambda player: player['Total Games Won'])
+        for player in ranked_list_of_dicts:
+            stats = player.values()
+            player_stats = list(stats)
+        
+        for list_player in player_stats:
+            scoreboard.append(list_player)
+        
+        return scoreboard
+
+    def display_highscore(scoreboard):
+        header = '''HIGHSCORE RESULTS:\n
+                    ------------------------\n
+                    PLAYER    | TOTAL WINS | TOTAL ROUNDS | 
+                    ---------------------------------------'''
+        print(header,)
+        for player in scoreboard:
+            return f'{player[0:3]}'
