575a5b63da7fc206ceef0005e0836e256279bfe6
enzotiberghien <enzotiberghien28@gmail.com>
2022-03-06 15:53:41 +0100

    Commit to pull

--- src/game/game.py
+++ src/game/game.py
@@ -3,18 +3,18 @@ import card, deck, cardHand, time
 
 class Game:
     def __init__(self, player_1, player_2):
+        self.game_status = True 
         self.player_1 = player_1
         self.player_2 = player_2
         self.p1_hand = None
         self.p2_hand = None
-        self.player_2_dis = "Computer" if self.player_2 == None else self.player_2        
 
 
     def print_card(self, p1_card, p2_card, print_info, pot):
         self.check_winner(self.p1_hand, self.p2_hand)
         p1_card, p2_card = self.p1_hand.pop(0), self.p2_hand.pop(0)
 
-        print(f"\n{f'{self.player_1}':<12}|{f' {self.player_2_dis}':<12}\n------------|------------" if print_info else "")
+        print(f"\n{f'{self.player_1}':<12}|{f' {self.player_2}':<12}\n------------|------------" if print_info else "")
         
         l1 = p1_card.ascii_card.split('\n')
         l2 = p2_card.ascii_card.split('\n')
@@ -47,25 +47,21 @@ class Game:
 
     def check_winner(self, p1_hand, p2_hand):
         if len(p1_hand) == 0:
-            print("\nPlayer 1 wins the game !")
+            print(f"\n{self.player_2} wins the game !")
             exit()
         elif len(p2_hand) == 0:
-            print("\nPlayer 2 wins the game !")
+            print(f"\n{self.player_1} wins the game !")
             exit()
-
-
-    def end(self):
-        return True
 
     
     def draw(self, simulate=False):
-        flag = True
-        while flag:
+        while True:
             pot = []
             p1_card, p2_card, winner = None, None, None
             p1_card, p2_card = self.print_card(p1_card, p2_card, True, pot)
 
-            while (p1_card.value == p2_card.value) or (p1_card.suit == p2_card.suit):
+            while ((p1_card.value == p2_card.value) or (p1_card.suit == p2_card.suit)) and self.game_status:
+                self.check_winner(self.p1_hand, self.p2_hand)
                 print("--- THERE IS A WAR ---")
 
                 if simulate: 
@@ -77,16 +73,16 @@ class Game:
                 p1_card, p2_card = self.print_card(p1_card, p2_card, False, pot)
 
             if p1_card.value > p2_card.value:
-                winner = "Player 1"
+                winner = self.player_1
                 [self.p1_hand.append(x) for x in pot]
             elif p1_card.value < p2_card.value:
-                winner = "Player 2"
+                winner = self.player_2
                 [self.p2_hand.append(x) for x in pot]
 
-            print(f"{winner} wins the round ! {self.player_1} has {len(self.p1_hand)} cards, {self.player_2_dis} has {len(self.p2_hand)} cards")
-
+            print(f"{winner} wins the round ! {self.player_1} has {len(self.p1_hand)} cards, {self.player_2} has {len(self.p2_hand)} cards")
             self.check_winner(self.p1_hand, self.p2_hand)
-            if not simulate: flag = False
+
+            if not simulate: break
 
 
     def start(self):
@@ -99,4 +95,3 @@ class Game:
             if res == "": self.draw()
             elif res.upper() == "EXIT": return True
             elif res.upper() == "CHEAT": self.draw(True)
-                
